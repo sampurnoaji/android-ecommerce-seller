b0705fc378cc264ac4402c1ef0f81621f6e35b49
Peter Sam <sam.krealogi@gmail.com>
2022-04-19 15:46:34 +0700

    login layout

---
+++ app/src/main/java/id/io/android/seller/presentation/user/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package id.io.android.seller.presentation.user.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.io.android.seller.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
