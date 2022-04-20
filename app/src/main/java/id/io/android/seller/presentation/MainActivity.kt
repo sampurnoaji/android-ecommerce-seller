250c564205974ca0dea327dc67f446c69c853a82
Peter Sam <sam.krealogi@gmail.com>
2022-04-20 21:07:09 +0700

    setup bottom nav

--- app/src/main/java/id/io/android/seller/presentation/MainActivity.kt
+++ app/src/main/java/id/io/android/seller/presentation/MainActivity.kt
@@ -3,9 +3,15 @@ package id.io.android.seller.presentation
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 import dagger.hilt.android.AndroidEntryPoint
+import id.io.android.seller.R
 import id.io.android.seller.core.BaseActivity
 import id.io.android.seller.databinding.ActivityMainBinding
+import id.io.android.seller.presentation.order.OrderFragment
+import id.io.android.seller.presentation.product.ProductFragment
+import id.io.android.seller.presentation.user.home.HomeFragment
 import id.io.android.seller.presentation.user.login.LoginActivity
 import id.io.android.seller.util.LoadState
 import id.io.android.seller.util.viewBinding
@@ -16,19 +22,54 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
     override val binding: ActivityMainBinding by viewBinding(ActivityMainBinding::inflate)
     override val vm: MainViewModel by viewModels()
 
+    private val homeFragment by lazy { HomeFragment() }
+    private val orderFragment by lazy { OrderFragment() }
+    private val productFragment by lazy { ProductFragment() }
+    private var currentFragment: Fragment = homeFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         checkLoggedInStatus()
+        setupFragments()
+    }
 
-        setupActionView()
-
-        vm.user.observe(this) {
-            when (it) {
-                is LoadState.Loading -> {}
-                is LoadState.Success -> binding.tv.text = it.data?.id.toString()
-                is LoadState.Error -> binding.tv.text = it.message
+    private fun setupFragments() {
+        showFragment(currentFragment)
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menuHome -> {
+                    showFragment(homeFragment)
+                    true
+                }
+                R.id.menuOrder -> {
+                    showFragment(orderFragment)
+                    true
+                }
+                R.id.menuProduct -> {
+                    showFragment(productFragment)
+                    true
+                }
+                else -> false
             }
         }
+    }
+
+    override fun onBackPressed() {
+        if (currentFragment != homeFragment) binding.bottomNavigation.selectedItemId = R.id.menuHome
+        else super.onBackPressed()
+    }
+
+    private fun showFragment(fragment: Fragment) {
+        with(supportFragmentManager) {
+            if (!fragment.isAdded) {
+                commit { add(R.id.container, fragment) }
+            }
+            commit {
+                hide(currentFragment)
+                show(fragment)
+            }
+        }
+        currentFragment = fragment
     }
 
     private fun checkLoggedInStatus() {
@@ -36,13 +77,6 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
-        }
-    }
-
-    private fun setupActionView() {
-        binding.logout.setOnClickListener {
-            vm.logout()
-            checkLoggedInStatus()
         }
     }
 }
